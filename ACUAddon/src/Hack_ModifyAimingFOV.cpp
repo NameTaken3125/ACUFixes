11bde941896369c93b4b66e03e3cfc9d5bb1a668
NameTaken <79968684+NameTaken3125@users.noreply.github.com>
2023-02-20 13:36:03 +0200

    Minor refactorings; rearranged code and comments in `Hack_ModifyAimingFOV.cpp`. `WhenCameraBlendingModeChanged_HijackConditionalFOVs()` now defers immediately to `FOVWhileAimingManager_AugmentedZoomOnRightClick::OnCameraModeChanged()`.

--- ACUAddon/src/Hack_ModifyAimingFOV.cpp
+++ ACUAddon/src/Hack_ModifyAimingFOV.cpp
@@ -52,6 +52,21 @@ float GetCurrentDeltaTime_UnpausedGame()
 }
 } // namespace ACU
 
+template<typename floatlike>
+floatlike simple_interp_using_game_time(floatlike mn, floatlike mx)
+{
+    auto now = ACU::GetCurrentTime_UnpausedGame();
+    float speed = 1.5f;
+    float interp = sin(now * speed);
+    interp = (interp + 1) / 2;
+    return mn + (mx - mn) * interp;
+}
+
+/*
+Stores references and initial values for all the points in FOVCurves for a given
+camera mode.
+Provides control over these values.
+*/
 class FOVCurveAccessor
 {
     std::vector<std::unique_ptr<AutoRestoredValue>> m_AutoRestoredValues;
@@ -76,10 +91,10 @@ public:
         SaveAllCurvePointsForFutureAccessAndRestoration(cameraMode);
     }
 private:
-    float m_CurrentEffectiveFOV;
+    float m_CachedCurrentEffectiveFOV;
 public:
-    float GetCurrentEffectiveFOV() { return m_CurrentEffectiveFOV; }
-    void SetEffectiveFOV(float value) { SetAllPointsInAllCurvesToConstantValue(value); m_CurrentEffectiveFOV = value; }
+    float GetCurrentEffectiveFOV() { return m_CachedCurrentEffectiveFOV; }
+    void SetEffectiveFOV(float value) { SetAllPointsInAllCurvesToConstantValue(value); m_CachedCurrentEffectiveFOV = value; }
 private:
     void SetAllPointsInAllCurvesToConstantValue(float value)
     {
@@ -125,71 +140,7 @@ constexpr float g_newFOVwhileAimingBomb = 1.0f; // = 1.5f;
 constexpr float g_newFOVwhileAimingBombFromBehindCover = 1.0f;
 constexpr float g_GoalFOVWhileAimingWithoutRMB = g_newFOVwhileAimingBomb;
 constexpr float g_GoalFOVWhileAimingAndPressingRMB = 0.60f;
-/*
-When player is aiming a bomb throw, the camera "tries to" follow the predicted landing position
-(actually, it seems to gradually follow a tracker that gradually follows the predicted landing position).
-The algorith that "follows" the predicted location is kinda jumpy.
-For example: if the player tries to aim upwards at a vertical pole (e.g. a thin chimney),
-then small left-right changes will cause the predicted landing location to jump on and off the pole,
-and so between on-the-pole and on-the-ground-behind-and-possibly-quite-far-below-the-pole.
-In addition to this, the camera's FOV decreases while aiming, and these sudden changes
-in the camera's tracked location mean big sudden changes in what's visible, resulting in disconnect between input (small mouse movement)
-and action (a lot of camera movement). Because the camera doesn't _strictly_ track the landing location,
-sudden changes also mean that in some cases the landing location is not even be visible.
-Same jankiness happens when aiming through a fence or something with holes in it wide enough
-for the predictor beam to pass through sometimes.
-It's all pretty annoying.
-While I don't have a fix for the tracking algorithm, I have found that simply not-decreasing the FOV
-results in aiming experience that is less jumpy and succeeds more often in keeping the landing location
-in sight.
-So this is the current fix:
-- I change the FOV values that are used under certain conditions.
-*/
-/*
-What I'm doing here:
-When starting to aim (gradual zoom-in period):
-[[[[[[[[[[[14521AAD0]+40]]+9c0]+18]+20]+2f0]]+8]+8]+6c] == float FOV for aim-throw-bomb mode.
-     [[[[[[14521AAD0]+40]]+9c0]+18]+20] == [[[ ACUCameraComponent +9c0]+18]+20] == `CameraData*`
-`CameraData` describes a "camera action" (for example, the gradual zoom when starting to aim).
-This pointer can be found can be found by finding corresponding _unchanging_ `CameraSelectorBlenderNode`.
-Specifically for the situation when the player is aiming with a bomb while standing (not behind cover),
-the node can be found in the registry of "ObjectRegistry_Entry" with a hash of `12F9251F30` (this hash gives only one result on memory scan).
 
-When aiming bomb from behind cover:
-- Hash == 0x34CE205063.
-- There are 4 HorizontalCurvePoints, which I think correspond to 4 cardinal directions relative to the cover direction.
-*/
-void WhenCameraBlendingModeChanged_HijackConditionalFOVs(AllRegisters* params)
-{
-    ObjectRegistry_Entry* newCameraMode = (ObjectRegistry_Entry*)params->rbx_;
-    if (newCameraMode->hash_mb == objHash_BombAimRegular)
-    {
-        auto& fovCurves = FOVCurvesDatabase::GetSingleton();
-        if (!fovCurves.curve_BombAim)
-        {
-            fovCurves.curve_BombAim = FOVCurveAccessor(newCameraMode);
-        }
-        fovCurves.curve_BombAim->SetEffectiveFOV(ACU::Input::IsPressedRMB() ? g_GoalFOVWhileAimingAndPressingRMB : g_GoalFOVWhileAimingWithoutRMB);
-    }
-    else if (newCameraMode->hash_mb == objHash_BombAimFromCover)
-    {
-        auto& fovCurves = FOVCurvesDatabase::GetSingleton();
-        if (!fovCurves.curve_BombAimFromBehindCover)
-        {
-            fovCurves.curve_BombAimFromBehindCover = FOVCurveAccessor(newCameraMode);
-        }
-        fovCurves.curve_BombAimFromBehindCover->SetEffectiveFOV(ACU::Input::IsPressedRMB() ? g_GoalFOVWhileAimingAndPressingRMB : g_GoalFOVWhileAimingWithoutRMB);
-    }
-}
-template<typename floatlike>
-floatlike simple_interp_using_game_time(floatlike mn, floatlike mx)
-{
-    auto now = ACU::GetCurrentTime_UnpausedGame();
-    float speed = 1.5f;
-    float interp = sin(now * speed);
-    interp = (interp + 1) / 2;
-    return mn + (mx - mn) * interp;
-}
 namespace Interpolations
 {
 
@@ -235,13 +186,21 @@ struct MethodLinear
 };
 } // namespace Interpolations
 #include "ACU/ACUPlayerCameraComponent.h"
+bool IsBombAimMode(ObjectRegistry_Entry* cameraMode)
+{
+    return cameraMode->hash_mb == objHash_BombAimRegular;
+}
+bool IsBombAimFromBehindCoverMode(ObjectRegistry_Entry* cameraMode)
+{
+    return cameraMode->hash_mb == objHash_BombAimFromCover;
+}
 bool IsInBombAimMode(ACUPlayerCameraComponent* cameraCpnt)
 {
-    return cameraCpnt->currentCameraSelectorBlenderNode->hash_mb == objHash_BombAimRegular;
+    return IsBombAimMode(cameraCpnt->currentCameraSelectorBlenderNode);
 }
 bool IsInBombAimFromBehindCoverMode(ACUPlayerCameraComponent* cameraCpnt)
 {
-    return cameraCpnt->currentCameraSelectorBlenderNode->hash_mb == objHash_BombAimFromCover;
+    return IsBombAimFromBehindCoverMode(cameraCpnt->currentCameraSelectorBlenderNode);
 }
 class FOVWhileAimingManager_AugmentedZoomOnRightClick
 {
@@ -266,6 +225,49 @@ public:
 
         cameraModeController.SetEffectiveFOV(newEffectiveFOV);
     }
+    /*
+    The FOV is determined by the current camera mode, specifically by
+    FOV Curves in corresponding CameraData.
+    By overriding the points in these curves I can control the effective FOV.
+    Here I determine the current camera mode, and if it occurs for the first time,
+    I store the references and initial values for the corresponding FOV Curves
+    so that I can later manually set them, as well as restore them to initial values
+    when the references are destroyed.
+
+    What I'm doing here:
+    When starting to aim (gradual zoom-in period):
+    [[[[[[[[[[[14521AAD0]+40]]+9c0]+18]+20]+2f0]]+8]+8]+6c] == float FOV for aim-throw-bomb mode.
+         [[[[[[14521AAD0]+40]]+9c0]+18]+20] == [[[ ACUCameraComponent +9c0]+18]+20] == `CameraData*`
+    `CameraData` describes a "camera action" (for example, the gradual zoom when starting to aim).
+    This pointer can be found can be found by finding corresponding _unchanging_ `CameraSelectorBlenderNode`.
+    Specifically for the situation when the player is aiming with a bomb while standing (not behind cover),
+    the node can be found in the registry of "ObjectRegistry_Entry" with a hash of `12F9251F30` (this hash gives only one result on memory scan).
+
+    When aiming bomb from behind cover:
+    - Hash == 0x34CE205063.
+    - There are 4 HorizontalCurvePoints, which I think correspond to 4 cardinal directions relative to the cover direction.
+    */
+    void OnCameraModeChanged(ObjectRegistry_Entry* newCameraMode)
+    {
+        if (IsBombAimMode(newCameraMode))
+        {
+            auto& fovCurves = FOVCurvesDatabase::GetSingleton();
+            if (!fovCurves.curve_BombAim)
+            {
+                fovCurves.curve_BombAim = FOVCurveAccessor(newCameraMode);
+            }
+            fovCurves.curve_BombAim->SetEffectiveFOV(ACU::Input::IsPressedRMB() ? g_GoalFOVWhileAimingAndPressingRMB : g_GoalFOVWhileAimingWithoutRMB);
+        }
+        else if (IsBombAimFromBehindCoverMode(newCameraMode))
+        {
+            auto& fovCurves = FOVCurvesDatabase::GetSingleton();
+            if (!fovCurves.curve_BombAimFromBehindCover)
+            {
+                fovCurves.curve_BombAimFromBehindCover = FOVCurveAccessor(newCameraMode);
+            }
+            fovCurves.curve_BombAimFromBehindCover->SetEffectiveFOV(ACU::Input::IsPressedRMB() ? g_GoalFOVWhileAimingAndPressingRMB : g_GoalFOVWhileAimingWithoutRMB);
+        }
+    }
 public:
     NON_MOVABLE(FOVWhileAimingManager_AugmentedZoomOnRightClick);
     FOVWhileAimingManager_AugmentedZoomOnRightClick() = default;
@@ -289,10 +291,42 @@ void UpdateConditionalFOVCurves(ACUPlayerCameraComponent* cameraCpnt)
         }
     }
 }
+
+
+
+
+
+
+
+void WhenCameraBlendingModeChanged_HijackConditionalFOVs(AllRegisters* params)
+{
+    ObjectRegistry_Entry* newCameraMode = (ObjectRegistry_Entry*)params->rbx_;
+    FOVWhileAimingManager_AugmentedZoomOnRightClick::GetSingleton().OnCameraModeChanged(newCameraMode);
+}
 void WhenCameraUpdateStarts(AllRegisters* params)
 {
     UpdateConditionalFOVCurves((ACUPlayerCameraComponent*)params->r14_);
 }
+/*
+When player is aiming a bomb throw, the camera "tries to" follow the predicted landing position
+(actually, it seems to gradually follow a tracker that gradually follows the predicted landing position).
+The algorith that "follows" the predicted location is kinda jumpy.
+For example: if the player tries to aim upwards at a vertical pole (e.g. a thin chimney),
+then small left-right changes will cause the predicted landing location to jump on and off the pole,
+and so between on-the-pole and on-the-ground-behind-and-possibly-quite-far-below-the-pole.
+In addition to this, the camera's FOV decreases while aiming, and these sudden changes
+in the camera's tracked location mean big sudden changes in what's visible, resulting in disconnect between input (small mouse movement)
+and action (a lot of camera movement). Because the camera doesn't _strictly_ track the landing location,
+sudden changes also mean that in some cases the landing location is not even be visible.
+Same jankiness happens when aiming through a fence or something with holes in it wide enough
+for the predictor beam to pass through sometimes.
+It's all pretty annoying.
+While I don't have a fix for the tracking algorithm, I have found that simply not-decreasing the FOV
+results in aiming experience that is less jumpy and succeeds more often in keeping the landing location
+in sight.
+So this is the current fix:
+- I change the FOV values that are used under certain conditions.
+*/
 ModifyConditionalFOVs::ModifyConditionalFOVs()
 {
     constexpr uintptr_t onCameraBlendingModeChanged = 0x141F4E4DE;
