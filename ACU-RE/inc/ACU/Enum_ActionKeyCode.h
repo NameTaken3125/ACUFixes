869324a4742bd69f20813bf231c786f3eb414495
NameTaken <79968684+NameTaken3125@users.noreply.github.com>
2023-03-02 15:36:08 +0200

    In ACU-RE, added `ActionKeyCode` enum.

---
+++ ACU-RE/inc/ACU/Enum_ActionKeyCode.h
@@ -0,0 +1,32 @@
+#pragma once
+
+enum class ActionKeyCode
+{
+    ParkourUp = 0x0,
+    LMB = 0x1,
+    EagleVision = 0x2,
+    ParkourDownInteract = 0x3,
+    Disguise = 0x4,
+    Numpad4CycleLeftHand = 0x5,
+    Medicine = 0x6,
+    Numpad6CycleBombs = 0x7,
+    Map = 0x8,
+    Escape = 0x9,
+    Crouch = 0xA,
+    RMB = 0xB,
+    Sprint = 0xC,
+    BombDrop = 0xD,
+    Reload = 0xE,
+    CenterCamera = 0xF,
+    SlotPhantom = 0x10,
+    SlotBerserk = 0x11,
+    SlotGun = 0x12,
+    SlotSmokeBomb = 0x13,
+    SlotStunBomb = 0x14,
+    SlotCherryBomb = 0x15,
+    SlotPoisonBomb = 0x16,
+    SlotMoneyPouch = 0x17,
+    Slot9_booster = 0x18,
+
+    ACTION_SET_SIZE = 0x20,
+};
--- ACU-RE/inc/ACU/InputContainer.h
+++ ACU-RE/inc/ACU/InputContainer.h
@@ -4,39 +4,8 @@
 #include "vmath/vmath.h"
 
 #include "HasInputContainers.h"
+#include "ActionKeyStates.h"
 
-class KeyStates
-{
-public:
-    unsigned char isPressed_parkourUp; //0x0000
-    unsigned char isPressed_LMB; //0x0001
-    unsigned char isPressed_eagleVision; //0x0002
-    unsigned char isPressed_parkourDownInteract; //0x0003
-    unsigned char isPressed_disguise; //0x0004
-    unsigned char isPressed_numpad4cycleLeftHand; //0x0005
-    unsigned char isPressed_medicine; //0x0006
-    unsigned char isPressed_numpad6cycleBombs; //0x0007
-    unsigned char isPressed_map; //0x0008
-    unsigned char isPressed_escape; //0x0009
-    unsigned char isPressed_crouch; //0x000A
-    unsigned char isPressed_RMB; //0x000B
-    unsigned char isPressed_sprint; //0x000C
-    unsigned char isPressed_bombDrop; //0x000D
-    unsigned char isPressed_reload; //0x000E
-    unsigned char isPressed_centerCamera; //0x000F
-    unsigned char isPressed_phantom; //0x0010
-    unsigned char isPressed_berserk; //0x0011
-    unsigned char isPressed_gun; //0x0012
-    unsigned char isPressed_smokeBomb; //0x0013
-    unsigned char isPressed_stunBomb; //0x0014
-    unsigned char isPressed_cherryBomb; //0x0015
-    unsigned char isPressed_poisonBomb; //0x0016
-    unsigned char isPressed_moneyPouch; //0x0017
-    unsigned char isPressed_9_booster; //0x0018
-    char pad_0019[7]; //0x0019
-
-}; //Size: 0x0020
-assert_sizeof(KeyStates, 0x20);
 
 class InputContainerBig;
 
