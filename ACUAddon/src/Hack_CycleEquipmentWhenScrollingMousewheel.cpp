10967833b056d5f334ce8b28cd433cdbaf6c13b0
NameTaken <79968684+NameTaken3125@users.noreply.github.com>
2023-03-02 15:41:01 +0200

    In "cycle equipment using mousewheel" hack, scrolling the mousewheel allows to cycle bombs in both directions. Warning! Not finished, doesn't account for locked equipment.

--- ACUAddon/src/Hack_CycleEquipmentWhenScrollingMousewheel.cpp
+++ ACUAddon/src/Hack_CycleEquipmentWhenScrollingMousewheel.cpp
@@ -36,7 +36,141 @@ HUDLetterReaderModule* FindHUDLetterReaderModule(UIWorldComponent& uiWorldCpnt)
     }
     return nullptr;
 }
+#include "ACU/Enum_EquipmentType.h"
+std::optional<ActionKeyCode> GetActionKeyCodeForEquipmentType(EquipmentType equipType)
+{
+    switch (equipType)
+    {
+    case EquipmentType::Pistol:
+    case EquipmentType::Rifle:
+    case EquipmentType::Blunderbuss:
+    case EquipmentType::GuillotineGun:
+        return ActionKeyCode::SlotGun;
+        break;
+    case EquipmentType::SmokeBomb:
+        return ActionKeyCode::SlotSmokeBomb;
+        break;
+    case EquipmentType::MoneyPouch:
+        return ActionKeyCode::SlotMoneyPouch;
+        break;
+    case EquipmentType::PoisonBomb:
+        return ActionKeyCode::SlotPoisonBomb;
+        break;
+    case EquipmentType::StunBomb:
+        return ActionKeyCode::SlotStunBomb;
+        break;
+    case EquipmentType::PhantomBlade:
+        return ActionKeyCode::SlotPhantom;
+        break;
+    case EquipmentType::BerserkBlade:
+        return ActionKeyCode::SlotBerserk;
+        break;
+    case EquipmentType::CherryBomb:
+        return ActionKeyCode::SlotCherryBomb;
+        break;
+    case EquipmentType::Slot9Booster:
+        return ActionKeyCode::Slot9_booster;
+        break;
+    default:
+        break;
+    }
+    return {};
+}
 
+constexpr inline std::array<EquipmentType, 6> g_BombTypes = {
+    EquipmentType::SmokeBomb,
+    EquipmentType::StunBomb,
+    EquipmentType::CherryBomb,
+    EquipmentType::PoisonBomb,
+    EquipmentType::MoneyPouch,
+    EquipmentType::Slot9Booster,
+};
+constexpr int GetEquipmentTypeIndexInCycle_Bombs(EquipmentType equipmentType)
+{
+    switch (equipmentType)
+    {
+    case SmokeBomb:
+        return 0;
+        break;
+    case StunBomb:
+        return 1;
+        break;
+    case CherryBomb:
+        return 2;
+        break;
+    case PoisonBomb:
+        return 3;
+        break;
+    case MoneyPouch:
+        return 4;
+        break;
+    case Slot9Booster:
+        return 5;
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+#include "ACU/BhvAssassin.h"
+#include "ACU/CharacterAI.h"
+#include "ACU/CSrvPlayerWeaponSwitch.h"
+CSrvPlayerWeaponSwitch* FindPlayerWeaponSwitch()
+{
+    BhvAssassin* bhvAssassin = BhvAssassin::GetSingletonPlayer();
+    if (!bhvAssassin) { return nullptr; }
+    CharacterAI* charAI = bhvAssassin->characterAI_toHealthAndInventory;
+    if (!charAI) { return nullptr; }
+
+    using vtbl_t = uint64;
+    constexpr vtbl_t CSrvPlayerWeaponSwitch_VTBL = 0x142F59810;
+    for (Statechart* stc : charAI->childStatecharts_mb_toHealth_60)
+    {
+        if (!stc) { continue; }
+        vtbl_t currentVtbl = *(vtbl_t*)stc;
+        if (currentVtbl == CSrvPlayerWeaponSwitch_VTBL)
+        {
+            return static_cast<CSrvPlayerWeaponSwitch*>(stc);
+        }
+    }
+    return nullptr;
+}
+EquipmentType GetCurrentEquipmentType_Bombs()
+{
+    CSrvPlayerWeaponSwitch* weaponSwitcher = FindPlayerWeaponSwitch();
+    if (!weaponSwitcher) { return EquipmentType::SmokeBomb; }
+    return weaponSwitcher->equipType_bombs;
+}
+int ProgressIndexInCycle(int currentIdx, int stepHowManyFromCurrent, size_t cycleSize)
+{
+    int result = (currentIdx + stepHowManyFromCurrent) % (int)cycleSize;
+    if (result < 0) { result += (int)cycleSize; }
+    return result;
+}
+EquipmentType ProgressEquipmentCycle_Bombs(int stepHowManyFromCurrentEquipment)
+{
+    EquipmentType currentBomb = GetCurrentEquipmentType_Bombs();
+    int currentBombSlotIdx = GetEquipmentTypeIndexInCycle_Bombs(currentBomb);
+    int newBombSlotIdx = ProgressIndexInCycle(currentBombSlotIdx, stepHowManyFromCurrentEquipment, g_BombTypes.size());
+    return g_BombTypes[newBombSlotIdx];
+}
+void InjectEquipmentSelection_ProgressFromCurrent(KeyStates& keyStatesThisFrame, int stepHowManyFromCurrentEquipment)
+{
+    EquipmentType progressedEquipmentCycle = ProgressEquipmentCycle_Bombs(stepHowManyFromCurrentEquipment);
+    std::optional<ActionKeyCode> progressedEquipmentSlot = GetActionKeyCodeForEquipmentType(progressedEquipmentCycle);
+    if (!progressedEquipmentSlot) { return; }
+    uint8& isNewSlotMarkedAsPressed = ((std::array<uint8, (size_t)ActionKeyCode::ACTION_SET_SIZE>&)keyStatesThisFrame)[(size_t)*progressedEquipmentSlot];
+    isNewSlotMarkedAsPressed = true;
+}
+void InjectEquipmentSelection_PreviousFromCurrent(KeyStates& keyStatesThisFrame)
+{
+    InjectEquipmentSelection_ProgressFromCurrent(keyStatesThisFrame, -1);
+}
+void InjectEquipmentSelection_NextFromCurrent(KeyStates& keyStatesThisFrame)
+{
+    InjectEquipmentSelection_ProgressFromCurrent(keyStatesThisFrame, 1);
+}
 void CycleEquipmentByScrollingMousewheel_BombsOnly(AllRegisters* params)
 {
     World* world = World::GetSingleton();
@@ -50,11 +184,10 @@ void CycleEquipmentByScrollingMousewheel_BombsOnly(AllRegisters* params)
     const bool isMWScrollUp = inpCont->IsMouseWheelScrollingUp();
     const bool isMWScrollDown = inpCont->IsMouseWheelScrollingDown();
 
-    uint8& isMarkedAsPressed_CycleBombs = inpCont->keyStates_thisFrame.isPressed_numpad6cycleBombs;
     if (isMWScrollUp)
-        isMarkedAsPressed_CycleBombs = true;
+        InjectEquipmentSelection_PreviousFromCurrent(inpCont->keyStates_thisFrame);
     if (isMWScrollDown)
-        isMarkedAsPressed_CycleBombs = true;
+        InjectEquipmentSelection_NextFromCurrent(inpCont->keyStates_thisFrame);
 }
 InputInjection_CycleEquipmentWhenScrollingMousewheel::InputInjection_CycleEquipmentWhenScrollingMousewheel()
 {
