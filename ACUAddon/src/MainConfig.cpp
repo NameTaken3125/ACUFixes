2617f285c9b939414f9693b6722f85315c5d48f1
NameTaken <79968684+NameTaken3125@users.noreply.github.com>
2023-03-05 10:59:37 +0200

    Reorganized MainConfig.cpp because I want to expose the config's JSON instance.

--- ACUAddon/src/MainConfig.cpp
+++ ACUAddon/src/MainConfig.cpp
@@ -10,6 +10,22 @@
 
 #include "base.h"
 
+namespace json {
+JSON FromFile(const fs::path& path)
+{
+    std::ifstream ifs(path);
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    std::string loadedString = ss.str();
+    return JSON::Load(loadedString);
+}
+void ToFile(const JSON& obj, const fs::path& path)
+{
+    std::ofstream ofs(path);
+    ofs << obj.dump();
+}
+} // namespace json
+
 
 namespace MainConfig {
 
@@ -43,18 +59,35 @@ void DumpConfig(JSON& cfg)
     WRITE_JSON_VARIABLE(cfg, imgui_showSuccessfulInjectionIndicator, BooleanAdapter);
     WRITE_JSON_VARIABLE(cfg, imgui_useImGui, BooleanAdapter);
 }
-void MainConfig::FindAndLoadConfigFileOrCreateDefault()
+namespace MainConfig {
+fs::path GetMainConfigFilepath()
 {
     std::string configFileName = "acufixes-config.json";
-    fs::path configFullPath = AbsolutePathInMyDirectory(configFileName);
-    JSONFileBridge configFile(configFullPath, true);
-    JSON& cfg = configFile.m_jsonObject;
+    return AbsolutePathInMyDirectory(configFileName);
+}
+JSON ReadMainConfigFile()
+{
+    fs::path configFullPath = GetMainConfigFilepath();
+    JSON cfg = json::FromFile(configFullPath);
     LOG_DEBUG("Read from config file \"%s\":\n%s\n", configFullPath.string().c_str(), cfg.dump().c_str());
+    return cfg;
+}
+JSON& Get()
+{
+    static JSON configFileJSON = ReadMainConfigFile();
+    return configFileJSON;
+}
+} // namespace MainConfig
+void MainConfig::FindAndLoadConfigFileOrCreateDefault()
+{
+    JSON& cfg = MainConfig::Get();
     if (cfg.IsObject())
     {
         READ_JSON_VARIABLE(cfg, imgui_showSuccessfulInjectionIndicator, BooleanAdapter);
         READ_JSON_VARIABLE(cfg, imgui_useImGui, BooleanAdapter);
     }
     DumpConfig(cfg);
+    fs::path configFullPath = GetMainConfigFilepath();
     LOG_DEBUG("Writing to config file \"%s\":\n%s\n", configFullPath.string().c_str(), cfg.dump().c_str());
+    json::ToFile(cfg, configFullPath);
 }
--- ACUAddon/src/MainConfig.h
+++ ACUAddon/src/MainConfig.h
@@ -1,10 +1,14 @@
 #pragma once
 
+namespace json { class JSON; }
+using json::JSON;
+
 namespace MainConfig {
 
 extern bool imgui_useImGui;
 extern bool imgui_showSuccessfulInjectionIndicator;
 
 void FindAndLoadConfigFileOrCreateDefault();
+JSON& Get();
 
 } // namespace MainConfig
