c2588ae5ad8bcaedcf4779bf5aab79017a600e8d
NameTaken <79968684+NameTaken3125@users.noreply.github.com>
2023-06-21 23:33:31 +0300

    In the "Removable Hood", refactored and added support for the "Napoleon Artillery Outfit", for which the bicorn hat's visibility is toggled, with hair visibility unchanged.

--- ACUFixes/src/Hack_RemovableHood.cpp
+++ ACUFixes/src/Hack_RemovableHood.cpp
@@ -70,19 +70,19 @@ struct HoodVariation
 {
     HoodVariation(
         const std::string& name
-        , uint64 handleHoodOn
-        , uint64 handleHoodOff
-        , std::optional<uint64> handleHoodOnExtra = {}
+        , uint64 handlePrimaryIdentifierWhenOn
+        , std::vector<uint64>&& handlesWhenOn
+        , std::vector<uint64>&& handlesWhenOff
     )
         : name(name)
-        , handle_hoodOn(handleHoodOn)
-        , handle_hoodOff(handleHoodOff)
-        , handle_hoodOnExtra(handleHoodOnExtra)
+        , handlePrimaryIdentifierWhenOn(handlePrimaryIdentifierWhenOn)
+        , handlesWhenOn(std::move(handlesWhenOn))
+        , handlesWhenOff(std::move(handlesWhenOff))
     {}
     std::string name;
-    uint64 handle_hoodOn;
-    uint64 handle_hoodOff;
-    std::optional<uint64> handle_hoodOnExtra;
+    uint64 handlePrimaryIdentifierWhenOn;
+    std::vector<uint64> handlesWhenOn;
+    std::vector<uint64> handlesWhenOff;
 };
 class HoodVariations
 {
@@ -91,7 +91,7 @@ public:
     {
         for (HoodVariation& hood : m_Hoods)
         {
-            if (hood.handle_hoodOn == handleHoodOn)
+            if (hood.handlePrimaryIdentifierWhenOn == handleHoodOn)
             {
                 return &hood;
             }
@@ -106,55 +106,55 @@ constexpr uint64 handle_hair1 = 0x17412AB3AC;
 constexpr uint64 handle_hair2 = 0x17412AB39C;
 constexpr uint64 handle_greyHairOfFranciadeHoods = 0x2318E0DEA8;
 HoodVariations g_Hoods({
-    HoodVariation( "CN_P_FR_Hood_Down_Arnaud_V1", 119263477692, 147798842981 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Arnaud_V2", 84857383766, 147798842259 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Arnaud_V3", 120094245449, 147798843584 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Arnaud_V4", 120094245753, 147798843762 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Rodeur_V1", 138594120216, 138594120217 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Rodeur_V2", 138594120220, 138594120219 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Rodeur_V3", 138594120218, 138594120213 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Rodeur_V4", 138594120215, 138594120214 ),
-    HoodVariation( "CN_P_FR_HoodDown_Revolutionnaire_V1", 102704136287, 199109962876 ),
-    HoodVariation( "CN_P_FR_HoodDown_Revolutionnaire_V2", 102704136472, 199109963034 ),
-    HoodVariation( "CN_P_FR_HoodDown_Revolutionnaire_V3", 102704136620, 200166425279 ),
-    HoodVariation( "CN_P_FR_HoodDown_Revolutionnaire_V4", 102704136768, 200166425449 ),
-    HoodVariation( "CN_P_FR_HoodDown_Napoleonian_V1", 136051788985, 146628658171 ),
-    HoodVariation( "CN_P_FR_HoodDown_Napoleonian_V2", 97499925652, 146628658936 ),
-    HoodVariation( "CN_P_FR_HoodDown_Napoleonian_V3", 136051789395, 146628659620 ),
-    HoodVariation( "CN_P_FR_HoodDown_Napoleonian_V4", 136051789853, 146628659894 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Phantom_V1", 132284718496, 161367850706 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Phantom_V2", 132284718507, 161893646994 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Phantom_V3", 132284718492, 161893647341 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Phantom_V4", 158617568238, 161893649233 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Musketeers_v1", 161893654071, 161893654073 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Musketeers_v2", 161893654101, 161893654072 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Musketeers_V3", 161893654102, 161893654074 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Musketeers_V4", 161893654099, 161893654100 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Medieval_V1", 161893651856, 161893651859 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Medieval_V2", 161893651854, 161893651855 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Medieval_V3", 161893651857, 161893651853 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Medieval_V4", 161893651860, 161893651858 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Military_V1", 161893654931, 161893654930 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Military_V2", 161893654928, 161893654929 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Military_V3", 161893654927, 161893654934 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Military_V4", 161893654932, 161893654933 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Brigands_V1", 161893653164, 161893653165 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Brigands_V2", 161893653240, 161893653241 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Brigands_V3", 161893653317, 161893653316 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Brigands_V4", 161893653393, 161893653392 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Noble_V1", 156675642145, 161893658204 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Noble_V2", 156675642146, 161893658202 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Noble_V3", 156675642128, 161893658201 ),
-    HoodVariation( "CN_P_FR_Hood_Down_Noble_V4", 156675642129, 161893658203 ),
-    HoodVariation( "McFarlane", 0x25AB1EB4A0, 0x25AB1EB4A1 ),
-    HoodVariation( "Connor", 0x234E8D4922, 0x234E8D4923 ),
-    HoodVariation( "Edward", 0x234E8D727E, 0x234E8D727D ),
-    HoodVariation( "Arno Fearless", 0x23DAB1789C, 0x2626CE15AE ),
-
-    HoodVariation( "Hood of Franciade", 0x3DA29AFE88, 0x3DA29AFE60, handle_greyHairOfFranciadeHoods ),
-    HoodVariation( "Hood of the Raiders", 0x3DA29AFD6B, 0x3DA29AFD85, handle_greyHairOfFranciadeHoods ),
-    HoodVariation( "Outfit - Franciade", 0x3DA29AFBF6, 0x3DA29AFB58, handle_greyHairOfFranciadeHoods ),
-    HoodVariation( "Outfit - Raider", 0x3DA29AFC85, 0x3DA29AFCA1, handle_greyHairOfFranciadeHoods ),
+    HoodVariation("CN_P_FR_Hood_Down_Arnaud_V1",         119263477692, {119263477692}, {147798842981, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Arnaud_V2",          84857383766, { 84857383766}, {147798842259, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Arnaud_V3",         120094245449, {120094245449}, {147798843584, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Arnaud_V4",         120094245753, {120094245753}, {147798843762, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Rodeur_V1",         138594120216, {138594120216}, {138594120217, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Rodeur_V2",         138594120220, {138594120220}, {138594120219, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Rodeur_V3",         138594120218, {138594120218}, {138594120213, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Rodeur_V4",         138594120215, {138594120215}, {138594120214, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_HoodDown_Revolutionnaire_V1", 102704136287, {102704136287}, {199109962876, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_HoodDown_Revolutionnaire_V2", 102704136472, {102704136472}, {199109963034, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_HoodDown_Revolutionnaire_V3", 102704136620, {102704136620}, {200166425279, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_HoodDown_Revolutionnaire_V4", 102704136768, {102704136768}, {200166425449, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_HoodDown_Napoleonian_V1",     136051788985, {136051788985}, {146628658171, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_HoodDown_Napoleonian_V2",      97499925652, { 97499925652}, {146628658936, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_HoodDown_Napoleonian_V3",     136051789395, {136051789395}, {146628659620, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_HoodDown_Napoleonian_V4",     136051789853, {136051789853}, {146628659894, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Phantom_V1",        132284718496, {132284718496}, {161367850706, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Phantom_V2",        132284718507, {132284718507}, {161893646994, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Phantom_V3",        132284718492, {132284718492}, {161893647341, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Phantom_V4",        158617568238, {158617568238}, {161893649233, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Musketeers_v1",     161893654071, {161893654071}, {161893654073, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Musketeers_v2",     161893654101, {161893654101}, {161893654072, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Musketeers_V3",     161893654102, {161893654102}, {161893654074, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Musketeers_V4",     161893654099, {161893654099}, {161893654100, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Medieval_V1",       161893651856, {161893651856}, {161893651859, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Medieval_V2",       161893651854, {161893651854}, {161893651855, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Medieval_V3",       161893651857, {161893651857}, {161893651853, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Medieval_V4",       161893651860, {161893651860}, {161893651858, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Military_V1",       161893654931, {161893654931}, {161893654930, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Military_V2",       161893654928, {161893654928}, {161893654929, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Military_V3",       161893654927, {161893654927}, {161893654934, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Military_V4",       161893654932, {161893654932}, {161893654933, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Brigands_V1",       161893653164, {161893653164}, {161893653165, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Brigands_V2",       161893653240, {161893653240}, {161893653241, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Brigands_V3",       161893653317, {161893653317}, {161893653316, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Brigands_V4",       161893653393, {161893653393}, {161893653392, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Noble_V1",          156675642145, {156675642145}, {161893658204, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Noble_V2",          156675642146, {156675642146}, {161893658202, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Noble_V3",          156675642128, {156675642128}, {161893658201, handle_hair1, handle_hair2}),
+    HoodVariation("CN_P_FR_Hood_Down_Noble_V4",          156675642129, {156675642129}, {161893658203, handle_hair1, handle_hair2}),
+    HoodVariation("McFarlane",                           0x25AB1EB4A0, {0x25AB1EB4A0}, {0x25AB1EB4A1, handle_hair1, handle_hair2}),
+    HoodVariation("Connor",                              0x234E8D4922, {0x234E8D4922}, {0x234E8D4923, handle_hair1, handle_hair2}),
+    HoodVariation("Edward",                              0x234E8D727E, {0x234E8D727E}, {0x234E8D727D, handle_hair1, handle_hair2}),
+    HoodVariation("Arno Fearless",                       0x23DAB1789C, {0x23DAB1789C}, {0x2626CE15AE, handle_hair1, handle_hair2}),
+    HoodVariation("Hood of Franciade",                   0x3DA29AFE88, {0x3DA29AFE88, handle_greyHairOfFranciadeHoods}, {0x3DA29AFE60, handle_hair1, handle_hair2}),
+    HoodVariation("Hood of the Raiders",                 0x3DA29AFD6B, {0x3DA29AFD6B, handle_greyHairOfFranciadeHoods}, {0x3DA29AFD85, handle_hair1, handle_hair2}),
+    HoodVariation("Outfit - Franciade",                  0x3DA29AFBF6, {0x3DA29AFBF6, handle_greyHairOfFranciadeHoods}, {0x3DA29AFB58, handle_hair1, handle_hair2}),
+    HoodVariation("Outfit - Raider",                     0x3DA29AFC85, {0x3DA29AFC85, handle_greyHairOfFranciadeHoods}, {0x3DA29AFCA1, handle_hair1, handle_hair2}),
+    HoodVariation("Napoleon Artillery Outfit",           264721070714, {264721070714}, {}),
     });
 
 Visual* ToVisual(Component* cpnt)
@@ -191,15 +191,10 @@ void ToggleHoodVisuals(HoodVariation& hood, bool doPutHoodOn)
 {
     Entity* player = ACU::GetPlayer();
     if (!player) { return; }
-    EnableVisibilityForVisualCpnt(*player, hood.handle_hoodOn, doPutHoodOn);
-    if (hood.handle_hoodOnExtra)
-    {
-        EnableVisibilityForVisualCpnt(*player, *hood.handle_hoodOnExtra, doPutHoodOn);
-    }
-
-    EnableVisibilityForVisualCpnt(*player, hood.handle_hoodOff, !doPutHoodOn);
-    EnableVisibilityForVisualCpnt(*player, handle_hair1, !doPutHoodOn);
-    EnableVisibilityForVisualCpnt(*player, handle_hair2, !doPutHoodOn);
+    for (uint64 handle : hood.handlesWhenOn)
+        EnableVisibilityForVisualCpnt(*player, handle, doPutHoodOn);
+    for (uint64 handle : hood.handlesWhenOff)
+        EnableVisibilityForVisualCpnt(*player, handle, !doPutHoodOn);
 }
 #include "ACU_SoundUtils.h"
 #include "ACU/ACUGlobalSoundSet.h"
