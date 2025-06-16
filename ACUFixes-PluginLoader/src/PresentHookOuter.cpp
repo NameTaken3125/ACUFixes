f383904d2754ae307c2228ac8b8264963f589b8c
NameTaken <79968684+NameTaken3125@users.noreply.github.com>
2025-06-16 21:38:57 +0300

    PresentHookOuter is made smaller once again.

--- ACUFixes-PluginLoader/src/PresentHookOuter.cpp
+++ ACUFixes-PluginLoader/src/PresentHookOuter.cpp
@@ -6,40 +6,12 @@
 
 #include "ACU/HasSwapchain.h"
 #include "ACU_DefineNativeFunction.h"
-// A full replacement leaves no allocated code in the call stack. This helps
-// the CrashLog generate a stacktrace.
-__int64 SwapchainPresentP_FullReplacement(HasSwapchain_6f8* hasSwapchain)
+
+HRESULT PRESENT_CALL SwapchainPresentCallReplacement(IDXGISwapChain* thisptr, UINT SyncInterval)
 {
-    struct Stack142305620
-    {
-        __int64 qword_0;
-        __int64 qword_8;
-        __int64 qword_10;
-        byte byte_18;
-        char pad_19[0x40 - 0x19];
-    };
-    assert_sizeof(Stack142305620, 0x40);
-    using Present_fnt = HRESULT(PRESENT_CALL*)(IDXGISwapChain* thisptr, UINT SyncInterval, UINT Flags);
-    DEFINE_GAME_FUNCTION(sub_1420FB6D0, 0x1420FB6D0, __int64, __fastcall, (Stack142305620 * a1, char a2));
-    DEFINE_GAME_FUNCTION(sub_1423032D0, 0x1423032D0, __int64, , ());
-    DEFINE_GAME_FUNCTION(sub_142303390, 0x142303390, int, __fastcall, (__int64 a1, HasSwapchain_6f8_0 * a2));
-    DEFINE_GAME_FUNCTION(sub_142129CC0, 0x142129CC0, void*, , ());
-    DEFINE_GAME_FUNCTION(sub_1421027B0, 0x1421027B0, __int64, __fastcall, (Stack142305620 * a1, void* a2));
-    DEFINE_GAME_FUNCTION(sub_1421027F0, 0x1421027F0, __int64, __fastcall, (Stack142305620 * a1));
-    Stack142305620 stackVar;
-    sub_1420FB6D0(&stackVar, 1);
-
-    Base::Hooks::GrabGraphicsDevicesInitializeImGuiAndDraw(hasSwapchain->swapchain);
-
-    hasSwapchain->swapchain->Present(hasSwapchain->swapchainPresent_syncInterval, 0);
-    sub_142303390(sub_1423032D0(), hasSwapchain->p0);
-    __int64 v4 = sub_1421027B0(&stackVar, sub_142129CC0());
-    stackVar.byte_18 = 0;
-    stackVar.qword_8 += v4;
-    stackVar.qword_10 = v4;
-    __int64 v5 = sub_1421027F0(&stackVar);
-    *(__int64*)0x14522BF08 = v5;
-    return v5;
+    Base::Hooks::GrabGraphicsDevicesInitializeImGuiAndDraw(thisptr);
+    const UINT Flags = 0;
+    return thisptr->Present(SyncInterval, Flags);
 }
 
 class PresentHookOuterPatch : public AutoAssemblerCodeHolder_Base
@@ -49,7 +21,23 @@ public:
 };
 PresentHookOuterPatch::PresentHookOuterPatch()
 {
-    PresetScript_ReplaceFunctionAtItsStart(0x142305620, SwapchainPresentP_FullReplacement);
+    DEFINE_ADDR(justBeforePresentIsCalled, 0x14230563F);
+    ALLOC(justBeforePresentIsCalled_cave, 0x80, 0x14230563F);
+
+    // I execute a "call" instead of "jmp" to the code cave, from which I "jmp" into C++.
+    // This way the return address pushed to the stack belongs to ACU.exe module
+    // and has a corresponding unwind information, which helps the CrashLog in generating a stacktrace.
+
+    // Total 6 bytes stolen; my custom Present()
+    // will return to 0x142305644.
+    justBeforePresentIsCalled = {
+        db(0xE8), RIP(justBeforePresentIsCalled_cave),
+        nop(1)
+    };
+    justBeforePresentIsCalled_cave = {
+        "ff 25 00 00 00 00"                                 //  - jmp    QWORD PTR [rip+0]
+        , dq((uintptr_t)SwapchainPresentCallReplacement)
+    };
 }
 
 static std::optional<AutoAssembleWrapper<PresentHookOuterPatch>> g_PresentOuterPatch;
